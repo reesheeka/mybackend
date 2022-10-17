7ad8b99c937ea76300718566f4b35af91de8664c
reesheeka <rishikasingh9525@gmail.com>
2022-10-18 00:03:14 +0530

    code added

--- src/introduction/intro.js
+++ src/introduction/intro.js
@@ -1,10 +1,23 @@
-const batchName = "Plutonium"
+// const batchName = "Plutonium"
 
-let printName = function() {
-    console.log('Bathc name is ', batchName)
+// let printName = function() {
+//     console.log('Bacth name is ', batchName)
+// }
+
+// module.exports.name = batchName
+// module.exports.printName = printName
+
+const movies = ['Kill Dill', 'Bhul Bhulaiya', '3 idiots', 'Raees']
+
+let printMovies = function() {
+    return movies
 }
 
-module.exports.name = batchName
-module.exports.printName = printName
+// module.exports.moviesName = printMovies
 
+// let printMoviesName = function() {
+//     return movies
+// }
 
+//module.exports.printMovies = printMovies
+module.exports = {printMovies}
