a79146e7d3dfe98dddb0e2587ce774bb10eaac57
reesheeka <rishikasingh9525@gmail.com>
2022-10-17 13:28:41 +0530

    done

---
+++ src/logger/logger.js
@@ -0,0 +1,17 @@
+// const url = "https://www.google.com"
+
+// let printSomething = function() {
+//     console.log("Request details are - a, b, c")
+//     return "done"
+// }
+
+// let abc = 2022
+// module.exports.myUrl = url
+// module.exports.myFunction = printSomething
+
+let printmyname = function(){
+    const res = "Welcome to my application. I am Rishika Singh and a part of FunctionUp Lithium Cohort."
+    return res
+}
+
+module.exports.welcome = printmyname
