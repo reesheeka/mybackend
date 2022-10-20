647325cdaad41933b33b4862287982914838b871
reesheeka <rishikasingh9525@gmail.com>
2022-10-20 08:38:16 +0530

    code addes

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const node = require('nodemon')
+const node = require('nodemon');
 
 //Q1. write an api which gives the missing number in an array of integers starting from 1….e.g [1,2,3,5,6,7]: 4 is missing
 // Your route code will look like this
@@ -8,7 +8,11 @@ router.get("/sol1", function (req, res) {
     //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
 
     let arr = [1, 2, 3, 5, 6, 7]
-    let n = arr.pop()
+    let total = 0
+    for (let i = 0; i < arr.length; i++) {
+        total = total + arr[i]
+    }
+    let n = arr[arr.length - 1]
     let sum = n * (n + 1) / 2
     let missingNumber = sum - total
 
@@ -29,7 +33,7 @@ router.get("/sol2", function (req, res) {
         total = total + arr[i]
     }
     let first = arr[0]
-    let last = arr.pop()
+    let last = arr[len - 1]
     let n = len + 1
     let sum = n * (first + last) / 2
     let missingNumber = sum - total
