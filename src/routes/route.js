e8094a4031994b3676d6cda5c67dbf0eb48dc0d2
reesheeka <rishikasingh9525@gmail.com>
2022-10-20 10:32:21 +0530

    code added

--- src/routes/route.js
+++ src/routes/route.js
@@ -2,44 +2,6 @@ const express = require('express');
 const abc = require('../introduction/intro')
 const router = express.Router();
 
-
-// router.get('/test-me', function (req, res) {
-//     console.log('My batch is', abc.name)
-//     abc.printName()
-//     res.send('My second ever api!')
-// });
-
-// router.get('/students', function (req, res){
-//     console.log("The path params in the request are : ", req.params)
-//     let students = ['Sabiha', 'Neha', 'Akash']
-//     res.send(students)
-// })
-
-// // Example 1 for path params
-// router.get('/students/:studentName', function(req, res){
-//     // ':' denotes that the following part of route is a variable
-//     // The value of this variable is what we are sending in the request url after /students
-//     // This value is set in the form of an object inside req.params
-//     // The object contain key value pairs
-//     // key is the variable in the route
-//     // value is whatever dynamic value sent in the request url
-//     let myParams = req.params
-
-//     // params attribute is fixed in a request object
-//     // params contains the path parameters object
-//     console.log("The path params in the request are : ", myParams)
-//     res.send('The full name is ' + myParams.studentName )
-// })
-
-// // Example 2 for path params
-//     router.get('/student-details/:name', function(req, res){
-//     let requestParams = req.params
-//     console.log("This is the request ", requestParams)
-//     let studentName = requestParams.name
-//     console.log('Name of the student is ', studentName)
-//     res.send('Dummy response')
-// })
-
 //1. Create an API for GET /movies that returns a list of movies. Define an array of movies in your code and return the value in response.
 
 router.get('/movies', function (req, res) {
@@ -49,12 +11,12 @@ router.get('/movies', function (req, res) {
 
 //2. Create an API GET /movies/:indexNumber (For example GET /movies/1 is a valid request and it should return the movie in your array at index 1). You can define an array of movies again in your api
 
-// router.get('/movies/:indexNumber', function(req, res){
-//    const movies = ['Kill Dil', 'Bhul Bhulaiya', '3 idiots', 'Raees']
-//    const myParams = req.params
-//    console.log('The path params in the request :', myParams)
-//    res.send(movies[Number(myParams.indexNumber)])
-// });
+router.get('/movies/:indexNumber', function(req, res){
+   const movies = ['Kill Dil', 'Bhul Bhulaiya', '3 idiots', 'Raees']
+   const myParams = req.params
+   console.log('The path params in the request :', myParams)
+   res.send(movies[Number(myParams.indexNumber)])
+});
 
 
 //3. Handle a scenario in problem 2 where if the index is greater than the valid maximum value a message is returned that tells the user to use a valid index in an error message.
@@ -122,7 +84,5 @@ router.get('/films/:filmId', function (req, res) {
         res.send(films[filmId - 1])
     }
 })
-
-
 
 module.exports = router;
