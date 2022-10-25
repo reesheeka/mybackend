e59621d2f572ed9c9bc1047e2983477a7c8a7cee
reesheeka <rishikasingh9525@gmail.com>
2022-10-25 11:40:01 +0530

    code added

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
+
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
