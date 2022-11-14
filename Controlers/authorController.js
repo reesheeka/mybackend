7ffeb3bb2692cac33f23f5c187f20989980acbb0
birendra8757 <computersinghaniya@gmail.com>
2022-11-14 15:34:00 +0530

    added authcontroller

---
+++ Controlers/authorController.js
@@ -0,0 +1,14 @@
+const authorModel=require("../Models/authorModel")
+
+
+const createAuthor = async(req,res)=>{
+    try{
+    let data=req.body;
+    let savedata =await authorModel.create(data)
+    res.status(201).send({data:savedata})
+    }catch(err){
+        res.status(500).send({error:err.message,status:false})
+    }
+
+}
+module.exports = {createAuthor}
